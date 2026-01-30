d0b4ea7f922ef1c022ebf976da706ecbc11bd428
Bence Gallai <bgg203@exeter.ac.uk>
2026-01-30 18:12:04 +0000

    AverageSquares completed with while loop

---
+++ AverageSquares.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; // We use the Scanner class from java.util to handle user input
+
+public class AverageSquares {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the start value: ");
+        int startNum = in.nextInt(); // Reads in an int from standard input
+        in.nextLine();
+        System.out.print("Please enter the end value: ");
+        int endNum = in.nextInt();
+
+        System.out.println("You entered: " + startNum + " and " + endNum);
+        int i = startNum;
+        int sum = 0;
+        while (i <= endNum){
+            sum += i*i;
+            i++;
+        }
+        double average = (double)sum / (endNum - startNum + 1);
+
+        System.out.println("Average of squares from " + startNum + " to " + endNum + ":");
+        System.out.println(average);
+
+        in.close();
+    }
+}
